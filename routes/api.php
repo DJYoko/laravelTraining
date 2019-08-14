be9be8830dd1d4357ef73c0a4c36dd1f0f0c2936
DJYoko <yokochan.recurrent@gmail.com>
2019-08-14 22:15:14 +0900

    add Vote Controller & Routing (created by artisan command , not all route will be used)

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:api'])->group(function () {
+    // "App\Http\Controllers\Admin"名前空間下のコントローラ
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::resource('votes', 'VoteController');
+
 });
