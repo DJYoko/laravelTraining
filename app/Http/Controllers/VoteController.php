988117d0a99840fb5388fec6fd82ce6725c658a4
DJYoko <yokochan.recurrent@gmail.com>
2019-08-16 22:21:11 +0900

    describe query builder for get Votes (not completed)

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vote;
+use Carbon\Carbon;
 
 class VoteController extends Controller
 {
@@ -13,7 +15,21 @@ class VoteController extends Controller
      */
     public function index()
     {
-        //
+
+        $currentDatetime = Carbon::now();
+
+        // pattern 1: start & end is defined
+        $query = Vote::where( 'votes.start_at', '<=', $currentDatetime );
+        $query = Vote::where( 'votes.end_at', '>=', $currentDatetime );
+
+        // pattern 2 : start & end is not defined
+        $query = Vote::whereIsNull( 'votes.start_at');
+        $query = Vote::whereIsNull( 'votes.end_at');
+
+        //TODO  settle pattern 1&2 as "orWhere"
+
+        $votes = $query->get();
+        return $answer;
     }
 
     /**
