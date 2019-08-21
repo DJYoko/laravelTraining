44cf5bcbbd712f346e5e892cda8556c5d0fade21
DJYoko <yokochan.recurrent@gmail.com>
2019-08-21 15:55:47 +0900

    remove unnecessary _constructor in vote controller

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -10,12 +10,6 @@ use Illuminate\Support\Facades\Auth;
 class VoteController extends Controller
 {
 
-    //コンストラクタ
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Display a listing of the resource.
      *
