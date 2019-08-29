f0b332374feb4ccb79ddecd7b10be7b1abb078bb
DJYoko <yokochan.recurrent@gmail.com>
2019-08-30 00:26:22 +0900

    add language Resource JSONs

--- resources/views/_layout/header.blade.php
+++ resources/views/_layout/header.blade.php
@@ -1,20 +1,38 @@
 <header class="bg-light">
   <div class="container">
-    <ul>
+    <ul class="nav">
       @if(Auth::check())
       <li class="nav-item">{{ \Auth::user()->name}}</li>
-      <li class="nav-item"><a href="{{ route('logout') }}">Logout</a></li>
+      <li class="nav-item"><a href="{{ route('logout') }}">{{ __('Logout') }}</a></li>
       @else
       <li class="nav-item">Guest User</li>
-      <li class="nav-item"><a href="{{ route('login') }}">Login</a></li>
-      <li class="nav-item"><a href="{{ route('register') }}">Register</a></li>
+      <li class="nav-item"><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
+      <li class="nav-item"><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
       @endif
+      <li class="nav-item">
+        <select id="set-lang">
+        <option value="en">English</option>
+        <option value="ja">日本語</option>
+        </select>
+      </li>
     </ul>
   </div>
 </header>
 <style>
+.nav {
+    margin-bottom: 20px;
+}
   .nav-item {
     display: inline-block;
     margin-left: 20px;
+    padding: 8px;
   }
 </style>
+<script>
+$(function(){
+    $('#set-lang').on('change', function(){
+        const self = $(this);
+        const selectedLanguage = self.val();
+    });
+});
+</script>
