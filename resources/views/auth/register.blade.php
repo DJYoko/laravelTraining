783401ae3c6d391a84ba9c200912a7b3f174d436
DJYoko <yokochan.recurrent@gmail.com>
2019-09-17 22:13:47 +0900

    add static texts

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,40 +4,40 @@
 .red{
     color:"f66"
 }</style>
-<title>Register</title>
+<title>{{__('Register')}}</title>
 @endsection
 @section('content')
 <div class="container">
-    <h1>Register</h1>
+    <h1>{{__('Register')}}</h1>
     <form action="/auth/register" method="post">
     {{csrf_field()}}
     <div class="list-group">
         <div class="list-group-item">
-            user name
+            {{__('UserName')}}
             <input type="text" name="name" size="30">
             <span class="red">{{ $errors->first('name')}}</span>
         </div>
 
         <div class="list-group-item">
-            email
+            {{__('Email')}}
             <input type="text" name="email" size="30">
             <span class="red">{{ $errors->first('email')}}</span>
         </div>
 
         <div class="list-group-item">
-            password
+            {{__('Password')}}
             <input type="text" name="password" size="30">
             <span class="red">{{ $errors->first('password')}}</span>
         </div>
 
         <div class="list-group-item">
-            password confirmation
+            {{__('PasswordConfirmation')}}
             <input type="text" name="password_confirmation" size="30">
             <span class="red">{{ $errors->first('password_confirmation')}}</span>
         </div>
     </div>
     <div class="text-center">
-        <button type="submit" value="send" class="btn btn-primary">Register</button>
+        <button type="submit" value="send" class="btn btn-primary">{{__('Register')}}</button>
     </div>
 </div>
 @endsection
