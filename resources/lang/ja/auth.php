41754f836ec0c24319ebbf0be77fcbad7207c49b
DJYoko <yokochan.recurrent@gmail.com>
2019-09-18 22:37:19 +0900

    add login error message resource of Japanese lang

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'ログイン情報が不正です',
+    'throttle' => '一定時間内に多数のアクセスがありました。時間をおいてご利用ください。',
+
+];
