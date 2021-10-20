72cd8c297ac0237765bc7bc54797abe42bbc924e
DJYoko <yokochan.recurrent@gmail.com>
2021-10-20 23:35:22 +0900

    filed [thumbnail_path] on circles become nullable

--- database/migrations/2021_10_19_132945_create_circles_table.php
+++ database/migrations/2021_10_19_132945_create_circles_table.php
@@ -17,7 +17,7 @@ class CreateCirclesTable extends Migration
             $table->increments('id');
             $table->string('name', 32);
             $table->string('path', 255);
-            $table->string('thumbnail_path', 255);
+            $table->string('thumbnail_path', 255)->nullable();
             $table->integer('create_user_id')->unsigned();
             $table->foreign('create_user_id')->references('id')->on('users');
             $table->timestamps();
