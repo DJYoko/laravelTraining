9407073cb6f7ae0771d0bd7b9689728b52acb0b5
DJYoko <yokochan.recurrent@gmail.com>
2021-10-20 23:10:15 +0900

    fix indent in header blade

--- resources/views/_layout/header.blade.php
+++ resources/views/_layout/header.blade.php
@@ -1,43 +1,43 @@
 <header class="bg-light">
-  <div class="container">
+<div class="container">
     <ul class="nav">
-      @if(Auth::check())
-      <li class="nav-item"><a href="{{ route('indexHome') }}">ホーム</a></li>
-      <li class="nav-item">{{ \Auth::user()->name}}</li>
-      <li class="nav-item"><a href="{{ route('logout') }}">{{ __('Logout') }}</a></li>
-      @else
-      <li class="nav-item">{{__('GuestUser')}}</li>
-      <li class="nav-item"><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
-      <li class="nav-item"><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
-      @endif
-      <li class="nav-item">
-        <select id="set-lang">
-        <option value="default">Lang/言語</option>
-        <option value="en">English</option>
-        <option value="ja">日本語</option>
-        </select>
-      </li>
+        @if(Auth::check())
+        <li class="nav-item"><a href="{{ route('indexHome') }}">ホーム</a></li>
+        <li class="nav-item">{{ \Auth::user()->name}}</li>
+        <li class="nav-item"><a href="{{ route('logout') }}">{{ __('Logout') }}</a></li>
+        @else
+        <li class="nav-item">{{__('GuestUser')}}</li>
+        <li class="nav-item"><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
+        <li class="nav-item"><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
+        @endif
+        <li class="nav-item">
+            <select id="set-lang">
+                <option value="default">Lang/言語</option>
+                <option value="en">English</option>
+                <option value="ja">日本語</option>
+            </select>
+        </li>
     </ul>
-  </div>
+</div>
 </header>
 <style>
 .nav {
-    margin-bottom: 20px;
+margin-bottom: 20px;
 }
-  .nav-item {
-    display: inline-block;
-    margin-left: 20px;
-    padding: 8px;
-  }
+.nav-item {
+display: inline-block;
+margin-left: 20px;
+padding: 8px;
+}
 </style>
 <script>
 $(function(){
-    $('#set-lang').on('change', function(){
-        const selectedLanguage = $(this).val();
-        if( selectedLanguage === 'default') {
-            return false;
-        }
-        location.href = "/lang/" + selectedLanguage;
-    });
+$('#set-lang').on('change', function(){
+    const selectedLanguage = $(this).val();
+    if( selectedLanguage === 'default') {
+        return false;
+    }
+    location.href = "/lang/" + selectedLanguage;
+});
 });
 </script>
