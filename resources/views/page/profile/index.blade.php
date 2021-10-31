21249afc8851d16df8fc33fc82eb9a83186a38ee
DJYoko <yokochan.recurrent@gmail.com>
2021-10-31 23:37:34 +0900

    profile edit page. assign bootstrap4 style classes.

--- resources/views/page/profile/index.blade.php
+++ resources/views/page/profile/index.blade.php
@@ -7,8 +7,17 @@
 <h1>プロフィール編集</h1>
     <form action="{{route('profile.edit')}}" method="POST">
         @csrf
-        名前<input name="userName" value="{{ \Auth::user()->name}}">
-        <button type="submit">送信</button>
+
+        <div class="form-group">
+            <label for="username">表示名</label>
+            <input type="text" class="form-control" id="username" aria-describedby="userNameHelp" placeholder="表示名" name="userName" value="{{ \Auth::user()->name}}">
+            <small id="userNameHelp" class="form-text text-muted">表示される名前を入力します</small>
+        </div>
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary">送信</button>
+        </div>
+
+
     </form>
 </div>
 @endsection
