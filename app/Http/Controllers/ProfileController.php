6212cc91c0b49f054c150d4abae619147fb951e9
DJYoko <yokochan.recurrent@gmail.com>
2021-11-01 20:58:46 +0900

    add thumbnail upload on profile edit page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,31 +9,42 @@ use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
-
-    public function detail(){
+    public function detail()
+    {
         return view('page.profile.index');
     }
 
     public function edit(Request $request)
     {
+        $user = Auth::user();
         $newName = $request->input('userName');
-        if (!is_null($newName)){
-            $user = Auth::user();
+        $thumbnailPath = '';
+
+        if ($file = $request->profileImage) {
+            $imageExtension = $request->file('profileImage')->extension();
+            $thumbnailPath = strval($user->id) . '_' .time() . '.' . $imageExtension;
+            $storagePath = public_path('/uploads/userProfile/');
+            $file->move($storagePath, $thumbnailPath);
+        }
+
+        if (!is_null($newName)) {
             DB::beginTransaction();
             try {
                 $user->name =$newName;
+                $user->thumbnail_path =$thumbnailPath;
                 $user->save();
                 DB::commit();
-            } catch ( \Exception $e ) {
+            } catch (\Exception $e) {
                 DB::rollBack();
-                return response()->json( [
+                return response()->json(
+                    [
                         'result' => 'error',
                         'message' => 'HTTP_STATUS_CODE_INTERNAL_SERVER_ERROR',
-                    ], config( 'constants.HTTP_STATUS_CODE_INTERNAL_SERVER_ERROR' )
+                    ],
+                    config('constants.HTTP_STATUS_CODE_INTERNAL_SERVER_ERROR')
                 );
             }
         }
         return view('page.profile.index');
     }
-
 }
--- resources/views/page/profile/index.blade.php
+++ resources/views/page/profile/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container">
 <h1>プロフィール編集</h1>
-    <form action="{{route('profile.edit')}}" method="POST">
+    <form action="{{route('profile.edit')}}" method="POST" enctype="multipart/form-data">
         @csrf
 
         <div class="form-group">
@@ -13,10 +13,17 @@
             <input type="text" class="form-control" id="username" aria-describedby="userNameHelp" placeholder="表示名" name="userName" value="{{ \Auth::user()->name}}">
             <small id="userNameHelp" class="form-text text-muted">表示される名前を入力します</small>
         </div>
+
+        <div class="form-group">
+            <label for="profileImage">アイコン画像</label>
+            <input type="file" name="profileImage">
+            <small id="profileImageHelp" class="form-text text-muted">表示される画像を登録します</small>
+            <div>現在登録されている画像</div>
+            <img src="/uploads/userProfile/{{ \Auth::user()->thumbnail_path}}" width="200" height="200">
+        </div>
         <div class="text-center">
             <button type="submit" class="btn btn-primary">送信</button>
         </div>
-
 
     </form>
 </div>
