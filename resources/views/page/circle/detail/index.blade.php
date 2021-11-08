ef628a6eb5ef1e020c022c0aba2e275e315eaf7d
DJYoko <yokochan.recurrent@gmail.com>
2021-11-08 20:57:43 +0900

    circle detail page. fix layout

--- resources/views/page/circle/detail/index.blade.php
+++ resources/views/page/circle/detail/index.blade.php
@@ -5,13 +5,19 @@
 @section('content')
     <div class="container">
     @if(!is_null($circle))
-        <h1>{{$circle->name}}</h1>
+        <div class="row">
+            <div class="col-md-9">
+                <h1>{{$circle->name}}</h1>
+            </div>
+            <div class="col-md-3">
+                @if($isEditable)
+                <p><a href="{{route('circle.update.input', ['circlePath' => $circle->path] )}}">サークル情報編集</a></p>
+                @endif
+            </div>
+        </div>
         <p>{{$circle->description}}</p>
-        <p>{{$circle->thumbnail_path}}</p>
     @endif
-    @if($isEditable)
-        <p><a href="{{route('circle.update.input', ['circlePath' => $circle->path] )}}">サークル情報編集</a></p>
-    @endif
+
     <h2>メンバー</h2>
     <div class="p-memberList">
         @foreach ($members as $member)
@@ -19,11 +25,9 @@
                 @if(isset($member->thumbnail_path))
                 style="background-image:url({{config( 'constants.USER_PROFILE_IMAGE_STORAGE_DIRECTORY' )}}{{ $member->thumbnail_path}});"
                 @endif
-                >
+            >
                 <a class="p-memberList__memberLink">
                     <div class="p-memberList__memberName">name: {{$member->name}}</div>
-
-                    {{-- <p>link: <a href="{{route('member.detail', ['memberPath' => $member->path])}}">{{route('member.detail', ['circlePath' => $circle->path])}}</a></p> --}}
                 </a>
             </div>
         @endforeach
