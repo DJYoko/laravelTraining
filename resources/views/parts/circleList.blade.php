a3a34f7f9705d41434a7d827af293524309222be
DJYoko <yokochan.recurrent@gmail.com>
2021-11-08 20:31:58 +0900

    circle list component. show circle thumbnail

--- resources/views/parts/circleList.blade.php
+++ resources/views/parts/circleList.blade.php
@@ -1,7 +1,11 @@
 <div class="p-circleList">
     @foreach ($_circles as $circle)
         <div class="p-circleList__circle">
-            <a href="{{route('circle.detail', ['circlePath' => $circle->path])}}" class="p-circleList__circleLink">
+            <a href="{{route('circle.detail', ['circlePath' => $circle->path])}}" class="p-circleList__circleLink"
+                @if($circle->thumbnail_path)
+                    style="background-image:url({{config( 'constants.CIRCLE_IMAGE_STORAGE_DIRECTORY' ) . $circle->thumbnail_path}});"
+                @endif
+            >
                 <p class="p-circleList__circleName">{{$circle->name}}</p>
             </a>
         </div>
