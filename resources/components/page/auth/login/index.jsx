27670c7cc38ca7ce556bf9a55f97dcd7e7c1fa1a
DJYoko <yokochan.recurrent@gmail.com>
2022-01-10 22:50:35 +0900

    replace login form from pure blade to React component

--- resources/components/page/auth/login/index.jsx
+++ resources/components/page/auth/login/index.jsx
@@ -2,12 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class PageAuthLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
   render() {
     return (
-      <p>resources\components\page\auth\login\index.jsx
-        <br />props.actionUrl {this.props.actionUrl}
-        <br />props.csrfToken {this.props.csrfToken}
-      </p>
+      <form action={this.props.actionUrl} method="post">
+          <input type="hidden" name="_token" value={this.props.csrfToken} />
+        <div className="list-group mb-2">
+          <div className="list-group-item">
+            Email
+            <input type="text" name="email" defaultValue={this.state.email} size="30" />
+          </div>
+
+          <div className="list-group-item">
+            Password
+            <input type="text" name="password" defaultValue={this.state.password} size="30" />
+          </div>
+        </div>
+        <div className="text-center">
+          <button type="submit" className="btn btn-primary">Login</button>
+        </div>
+      </form>
     );
   }
 }
--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -18,27 +18,11 @@
         @endforeach
       </div>
     @endif
-    <form action="/auth/login" method="post">
-      {{ csrf_field() }}
-      <div class="list-group mb-2">
-        <div class="list-group-item">
-          {{ __('Email') }}
-          <input type="text" name="email" size="30">
-        </div>
+    <div id="react-loginForm" data-csrf-token="{{ csrf_token() }}" data-action-url="{{ route('login') }}">
+      {{-- React build test --}}
+    </div>
+  </div>
 
-        <div class="list-group-item">
-          {{ __('Password') }}
-          <input type="text" name="password" size="30">
-        </div>
-      </div>
-      <div class="text-center">
-        <button type="submit" value="send" class="btn btn-primary">{{ __('Login') }}</button>
-      </div>
-    </form>
-  </div>
-  <div id="react-loginForm" data-csrf-token="{{ csrf_token() }}" data-action-url="{{ route('login') }}">
-    {{-- React build test --}}
-  </div>
   <script src="/js/page/auth/login/index.js"></script>
 
 @endsection
