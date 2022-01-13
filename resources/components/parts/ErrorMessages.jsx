6db64768bb9e7de7bc21b50891dddcac218e4573
DJYoko <yokochan.recurrent@gmail.com>
2022-01-13 21:53:02 +0900

    add error Message component

---
+++ resources/components/parts/ErrorMessages.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ErrorMessages extends React.Component {
+  render() {
+    if (this.props.messages.length === 0) {
+      return null;
+    }
+
+    return (
+      <p className="alert alert-danger">{this.props.messages.join('\n')}</p>
+    );
+  }
+}
+
+ErrorMessages.propTypes = {
+  messages: PropTypes.array,
+};
+
+ErrorMessages.defaultProps = {
+  messages: [],
+};
+
+export default ErrorMessages;
