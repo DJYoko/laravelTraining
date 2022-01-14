0ca0bbb68626b074e16bd48849acd6ce5eac0230
DJYoko <yokochan.recurrent@gmail.com>
2022-01-14 21:09:14 +0900

    Register Page - apply Bootstrap style classes

--- resources/views/auth/register/index.blade.php
+++ resources/views/auth/register/index.blade.php
@@ -4,10 +4,9 @@
 @endsection
 @section('content')
 <div class="container">
-  <h1>{{ __('Register') }}</h1>
+  <h1 class="h1 mb-3">{{ __('Register') }}</h1>
   <div class="list-group">
     <div class="list-group-item">
-
       <div id="react-registerForm" data-csrf-token="{{ csrf_token() }}" data-action-url="{{ route('register') }}"
         data-name="{{ old('name') }}" data-email="{{ old('email') }}"
         data-error-messages="@include('parts.errorMessagesJson', ['displayErrors' => $errors->getMessages()])">
