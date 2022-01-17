abd0c29e3d6ad1422b9f20bc1772a672b7f2d2cf
DJYoko <yokochan.recurrent@gmail.com>
2022-01-17 21:47:43 +0900

    switch DUSK env settings by using .env.dusk.testing

--- config/database.php
+++ config/database.php
@@ -53,21 +53,6 @@ return [
       'strict' => true,
       'engine' => null,
     ],
-    'mysqlTest' => [ // for DUSK test
-      'driver' => 'mysql',
-      'host' => env('DB_HOST', '127.0.0.1'),
-      'port' => env('DB_PORT', '3306'),
-      'database' => env('DB_DATABASE', 'forge'),
-      'username' => env('DB_USERNAME', 'forge'),
-      'password' => env('DB_PASSWORD', ''),
-      'unix_socket' => env('DB_SOCKET', ''),
-      'charset' => 'utf8mb4',
-      'collation' => 'utf8mb4_unicode_ci',
-      'prefix' => '',
-      'strict' => false,
-      'engine' => null,
-    ],
-
     'pgsql' => [
       'driver' => 'pgsql',
       'host' => env('DB_HOST', '127.0.0.1'),
