283907ff09de3c643edaff2f6407571ea140ff1a
DJYoko <yokochan.recurrent@gmail.com>
2022-01-18 14:15:27 +0900

    add dusk-attribute for detecting login-complete

--- resources/views/page/home/index.blade.php
+++ resources/views/page/home/index.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
 <div class="container">
-<h1>ログインしました</h1>
-    <ul>
-        <li><a href="{{route('member.update.input')}}">プロフィール編集</a></li>
-        <li><a href="{{route('circle.create.input')}}">サークル登録</a></li>
-    </ul>
-    <h2>参加しているサークル</h2>
-    @include('parts.circleList', ['_circles' => $myCircles])
+  <h1 dusk="memberHomePageTitle">ログインしました</h1>
+  <ul>
+    <li><a href="{{route('member.update.input')}}">プロフィール編集</a></li>
+    <li><a href="{{route('circle.create.input')}}">サークル登録</a></li>
+  </ul>
+  <h2>参加しているサークル</h2>
+  @include('parts.circleList', ['_circles' => $myCircles])
 
 </div>
 @endsection
