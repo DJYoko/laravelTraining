157bce42a95e9ccc097b5080d1f427a7172f9199
DJYoko <yokochan.recurrent@gmail.com>
2022-01-21 22:20:03 +0900

    add testing for login success

--- tests/Browser/Testing/LoginTest.php
+++ tests/Browser/Testing/LoginTest.php
@@ -50,4 +50,38 @@ class LoginTest extends DuskTestCase
       $browser->assertSee('The password field is required.');
     });
   }
+
+  /**
+   * @return void
+   */
+  public function testLoginSuccess()
+  {
+    $randomStr = $this->generateRandomString(8);
+    $tempMailAddress = $randomStr . '@testDomain.com';
+
+    // create testing user
+    User::create([
+      'name' => $randomStr,
+      'email' => $tempMailAddress,
+      'password' => bcrypt('password'),
+    ]);
+
+    $this->browse(function (Browser $browser) use ($tempMailAddress) {
+
+      // visit page.
+      // elements are shown
+      $browser->visit(route('login'))
+        ->assertTitle('Login')
+        ->waitFor('form');
+
+      // login as testing user
+      $browser->value('@loginFormInputEmail', $tempMailAddress);
+      $browser->value('@loginFormInputPassword', 'password');
+      $browser->press('@loginFormButtonSubmit');
+
+      $url = $browser->driver->getCurrentURL();
+      $expectedUrl = route('indexHome');
+      $this->assertEquals($expectedUrl, $url);
+    });
+  }
 }
