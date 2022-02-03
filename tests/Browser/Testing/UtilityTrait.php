801786e6ff29be134567f49a4691782b7aa441ff
DJYoko <yokochan.recurrent@gmail.com>
2022-02-03 20:46:54 +0900

    Dusk Test - add utility function loginAsTestingUser

--- tests/Browser/Testing/UtilityTrait.php
+++ tests/Browser/Testing/UtilityTrait.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Browser\Testing;
 
+use App\Models\User;
+use Laravel\Dusk\Browser;
+
 trait UtilityTrait
 {
   /**
@@ -16,5 +19,36 @@ trait UtilityTrait
       $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
+  }
+
+  public function loginAsTestingUser()
+  {
+    $testUserName = config('testing.userName');
+    $testUserEmail = config('testing.userEmail');
+    $testUserPasswordInput = config('testing.userPasswordInput');
+
+    $testUser = User::where('users.email', '=', $testUserEmail)->first();
+    if (is_null($testUser)) {
+      // create testing user
+      User::create([
+        'name' => $testUserName,
+        'email' => $testUserEmail,
+        'password' => bcrypt($testUserPasswordInput),
+      ]);
+    }
+
+    $this->browse(function (Browser $browser) use ($testUserEmail, $testUserPasswordInput) {
+
+      // visit page.
+      // elements are shown
+      $browser->visit(route('login'))
+        ->assertTitle('Login')
+        ->waitFor('form');
+
+      // login as testing user
+      $browser->value('@loginFormInputEmail', $testUserEmail);
+      $browser->value('@loginFormInputPassword', $testUserPasswordInput);
+      $browser->press('@loginFormButtonSubmit');
+    });
   }
 }
