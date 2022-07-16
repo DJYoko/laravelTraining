8626c9e47651d11093eaff8f469e473e029e5000
DJYoko <yokochan.recurrent@gmail.com>
2022-07-16 22:58:13 +0900

    source code format (blade files)

--- resources/views/_layout/head.blade.php
+++ resources/views/_layout/head.blade.php
@@ -1,5 +1,4 @@
 <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-  integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway:100,600">
--- resources/views/parts/circleList.blade.php
+++ resources/views/parts/circleList.blade.php
@@ -1,17 +1,16 @@
 <div class="p-circleList">
-    @foreach ($_circles as $circle)
-        <div class="p-circleList__circle">
-            <a href="{{route('circle.detail', ['circlePath' => $circle->path])}}" class="p-circleList__circleLink"
-                @if($circle->thumbnail_path)
-                    style="background-image:url({{config( 'constants.CIRCLE_IMAGE_STORAGE_DIRECTORY' ) . $circle->thumbnail_path}});"
-                @endif
-            >
-                <p class="p-circleList__circleName">
-                  <span class="p-circleList__circleNameText">
-                    {{$circle->name}}
-                  </span>
-                </p>
-            </a>
-        </div>
-    @endforeach
+  @foreach ($_circles as $circle)
+  <div class="p-circleList__circle">
+    <a href="{{route('circle.detail', ['circlePath' => $circle->path])}}" class="p-circleList__circleLink" @if($circle->thumbnail_path)
+      style="background-image:url({{config( 'constants.CIRCLE_IMAGE_STORAGE_DIRECTORY' ) . $circle->thumbnail_path}});"
+      @endif
+      >
+      <p class="p-circleList__circleName">
+        <span class="p-circleList__circleNameText">
+          {{$circle->name}}
+        </span>
+      </p>
+    </a>
+  </div>
+  @endforeach
 </div>
