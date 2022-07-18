95b51176f65d625a9939dab9a1665f71b7484787
DJYoko <yokochan.recurrent@gmail.com>
2022-07-18 21:28:39 +0900

    source code format (route.php)

--- routes/web.php
+++ routes/web.php
@@ -23,23 +23,23 @@ Route::post('auth/login', 'Auth\LoginController@login');
 Route::get('auth/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::prefix('circle')->group(function () {
-    Route::get('/', 'CircleController@index')->name('circle.index');
-    Route::get('/detail/{circlePath}', 'CircleController@circleDetail')->name('circle.detail');
+  Route::get('/', 'CircleController@index')->name('circle.index');
+  Route::get('/detail/{circlePath}', 'CircleController@circleDetail')->name('circle.detail');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/home', 'IndexController@home')->name('indexHome');
-    Route::get('/vote', 'VoteController@home')->name('voteIndex');
+  Route::get('/home', 'IndexController@home')->name('indexHome');
+  Route::get('/vote', 'VoteController@home')->name('voteIndex');
 
-    Route::prefix('member')->group(function () {
-        Route::get('/edit', 'MemberController@updateInput')->name('member.update.input');
-        Route::post('/edit', 'MemberController@updateSave')->name('member.update.save');
-    });
+  Route::prefix('member')->group(function () {
+    Route::get('/edit', 'MemberController@updateInput')->name('member.update.input');
+    Route::post('/edit', 'MemberController@updateSave')->name('member.update.save');
+  });
 
-    Route::prefix('circle')->group(function () {
-        Route::get('/create', 'CircleController@createInput')->name('circle.create.input');
-        Route::post('/create', 'CircleController@createSave')->name('circle.create.save');
-        Route::get('/update/{circlePath}', 'CircleController@updateInput')->name('circle.update.input');
-        Route::post('/update/{circlePath}', 'CircleController@updateSave')->name('circle.update.save');
-    });
+  Route::prefix('circle')->group(function () {
+    Route::get('/create', 'CircleController@createInput')->name('circle.create.input');
+    Route::post('/create', 'CircleController@createSave')->name('circle.create.save');
+    Route::get('/update/{circlePath}', 'CircleController@updateInput')->name('circle.update.input');
+    Route::post('/update/{circlePath}', 'CircleController@updateSave')->name('circle.update.save');
+  });
 });
